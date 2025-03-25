07c0d7786e0f4bf26feae9a280c84ba14ca3b139
Kevin Young <kevindyoung76@gmail.com>
2025-03-24 22:44:12 -0400

    update metrics.js

--- src/metrics/metrics.js
+++ src/metrics/metrics.js
@@ -4,85 +4,51 @@ const os = require('os');
 const configImport = require('../config.js');
 const config = configImport.__esModule ? configImport.default : configImport;
 
+const { getActiveUserMetrics } = require("./metricTypes/activeUserMetrics");
+const { getAuthMetrics } = require("./metricTypes/authMetrics");
+const { getHttpMetrics } = require("./metricTypes/httpMetrics");
+const { getLatencyMetrics } = require("./metricTypes/latencyMetrics");
+const { getPizzaMetrics } = require("./metricTypes/pizzaMetrics");
+const { getSystemMetrics } = require("./metricTypes/systemMetrics");
 
-const { url, apiKey } = config.metrics;
+const sendMetrics = async () => {
+  try {
+    const metrics = {
+      resourceMetrics: [
+        {
+          scopeMetrics: [
+            getHttpMetrics(),
+            getSystemMetrics(),
+            await getActiveUserMetrics(),
+            getAuthMetrics(),
+            getPizzaMetrics(),
+            getLatencyMetrics(),
+          ],
+        },
+      ],
+    };
 
-let requests = 0;
-let latency = 0;
-
-setInterval(() => {
-  const cpuValue = Math.floor(Math.random() * 100) + 1;
-  sendMetricToGrafana('cpu', cpuValue, 'gauge', '%');
-
-  requests += Math.floor(Math.random() * 200) + 1;
-  sendMetricToGrafana('requests', requests, 'sum', '1');
-
-  latency += Math.floor(Math.random() * 200) + 1;
-  sendMetricToGrafana('latency', latency, 'sum', 'ms');
-}, 1000);
-
-function sendMetricToGrafana(metricName, metricValue, type, unit) {
-  const metric = {
-    resourceMetrics: [
-      {
-        scopeMetrics: [
-          {
-            metrics: [
-              {
-                name: metricName,
-                unit: unit,
-                [type]: {
-                  dataPoints: [
-                    {
-                      asInt: metricValue,
-                      timeUnixNano: Date.now() * 1000000,
-                    },
-                  ],
-                },
-              },
-            ],
-          },
-        ],
+    const response = await fetch(config.grafana.url, {
+      method: "POST",
+      body: JSON.stringify(metrics),
+      headers: {
+        Authorization: `Bearer ${config.grafana.apiKey}`,
+        "Content-Type": "application/json",
       },
-    ],
-  };
-
-  if (type === 'sum') {
-    metric.resourceMetrics[0].scopeMetrics[0].metrics[0][type].aggregationTemporality = 'AGGREGATION_TEMPORALITY_CUMULATIVE';
-    metric.resourceMetrics[0].scopeMetrics[0].metrics[0][type].isMonotonic = true;
-  }
-
-  const body = JSON.stringify(metric);
-  fetch(`${url}`, {
-    method: 'POST',
-    body: body,
-    headers: { Authorization: `Bearer ${apiKey}`, 'Content-Type': 'application/json' },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        response.text().then((text) => {
-          console.error(`Failed to push metrics data to Grafana: ${text}\n${body}`);
-        });
-      } else {
-        console.log(`Pushed ${metricName}`);
-      }
-    })
-    .catch((error) => {
-      console.error('Error pushing metrics:', error);
     });
-}
 
+    if (!response.ok) {
+      console.error("Failed to push metrics data to Grafana", response);
+    }
+  } catch (error) {
+    console.error("Error pushing metrics:", error);
+  }
+};
 
+const periodicallySendMetrics = () => {
+  setInterval(sendMetrics, 5000);
+};
 
-function getCpuUsagePercentage() {
-  const cpuUsage = os.loadavg()[0] / os.cpus().length;
-  return cpuUsage.toFixed(2) * 100;
-}
-
-function getMemoryUsagePercentage() {
-  const totalMemory = os.totalmem();
-  const freeMemory = os.freemem();
-  const usedMemory = totalMemory - freeMemory;
-  const memoryUsage = (usedMemory / totalMemory) * 100;
-  return memoryUsage.toFixed(2);
-}
+module.exports = {
+  periodicallySendMetrics,
+};
