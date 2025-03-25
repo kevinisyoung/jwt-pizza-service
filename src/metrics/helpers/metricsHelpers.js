e07815e2018f7746b31a9e979b810b8a98d81bdd
Kevin Young <kevindyoung76@gmail.com>
2025-03-24 22:09:57 -0400

    Proper metrics accessing

--- src/metrics/helpers/metricsHelpers.js
+++ src/metrics/helpers/metricsHelpers.js
@@ -2,16 +2,10 @@ const configImport = require("../../config");
 // Handle both ES module (with default) and CommonJS formats
 const config = configImport.__esModule ? configImport.default : configImport;
 
-const freak_dude_1 = config;
-const freak_dude_2 = config.grafana;
-const freak_dude_3 = config.grafana.source;
-console.log("config:", config);
-console.log("config.grafana:", config.grafana);
-console.log("config.grafana.source:", config.grafana.source);
 
 const sourceAttribute = {
   key: "source",
-  value: { stringValue: config.grafana.source },
+  value: { stringValue: config.metrics.source },
 };
 
 const isApiRequest = (req) => {
